89818b06ae696d343dc0961e0a036416c3b0da3a
Leonid <l.cherepnin@yandex.ru>
2017-12-29 18:20:21 +0300

    End a Section Subscribe, Add a Function Click For Display This Section

--- app/js/script.js
+++ app/js/script.js
@@ -1,8 +1,21 @@
-var menu = document.getElementById('menu'),
-    input = document.getElementById('btnMenu');
+//MOBILE MENU
+(function () {
+  var menu = document.getElementById('menu'),
+      input = document.getElementById('btnMenu');
 
 
 
-input.onclick = function () {
-  menu.classList.toggle('show');
-};
+  input.onclick = function () {
+    menu.classList.toggle('show');
+  };
+})();
+
+//SECTION AFTER HEADER DISPLAY
+(function () {
+  var click = document.getElementById('open'),
+      section = document.getElementById('dropdown');
+
+      click.addEventListener('click', function() {
+         section.style.display = (section.style.display == 'block') ? 'none' : 'block';
+      });
+})();
