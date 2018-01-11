9922e245df82c43c99d8d5b78dc8d12c0ee4088d
Leonid <l.cherepnin@yandex.ru>
2018-01-11 19:10:25 +0300

    Add a slider after work

--- app/js/script.js
+++ app/js/script.js
@@ -19,3 +19,30 @@
          section.style.display = (section.style.display == 'block') ? 'none' : 'block';
       });
 })();
+
+//SLIDER
+(function () {
+  document.getElementById('slider-left').onclick = sliderLeft;
+  document.getElementById('slider-right').onclick = sliderRight;
+  var left = 0;
+  var polosa = document.getElementById('polosa');
+
+  function sliderLeft(){
+  	left = left - 891;
+  	if (left < - 1782) {
+  		left = 0;
+  	}
+  	polosa.style.left = left + 'px';
+  }
+
+  function sliderRight() {
+  	left = left + 891;
+  	if (left > 0) {
+  		left = - 1782;
+  	}
+  	polosa.style.left = left + 'px';
+  }
+
+  setInterval(sliderLeft, 5000);
+
+})();
