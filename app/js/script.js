7bd17907b63f55fdf5f22da9225ef15794afc1a2
Leonid <l.cherepnin@yandex.ru>
2018-01-15 20:04:10 +0300

    Add a footer section footer-up

--- app/js/script.js
+++ app/js/script.js
@@ -1,31 +1,3 @@
-//OUR-TEAM HOVER CRUMBS
-
-(function () {
-  var li1 = document.getElementById('li1'),
-      li2 = document.getElementById('li2'),
-      li3 = document.getElementById('li3'),
-      li4 = document.getElementById('li4');
-
-  var crumb1 = document.getElementById('crumb1'),
-      crumb2 = document.getElementById('crumb2'),
-      crumb3 = document.getElementById('crumb3'),
-      crumb4 = document.getElementById('crumb4');
-
-      // crumb1.style.background = 'red';
-
-  li1.onmouseover = function () {
-    crumb1.style.width = '15px';
-    crumb1.style.height = '15px';
-    crumb1.style.background = '#f7600e';
-  };
-  li1.onmouseout =function () {
-    crumb1.style.width = '';
-    crumb1.style.height = '';
-    crumb1.style.background = '';
-  };
-
-})();
-
 //MOBILE MENU
 (function () {
   var menu = document.getElementById('menu'),
@@ -72,5 +44,42 @@
   }
 
   setInterval(sliderLeft, 5000);
+
+})();
+
+
+
+//OUR-TEAM HOVER CRUMBS
+(function () {
+  var li1 = document.getElementById('li1'),
+        li2 = document.getElementById('li2'),
+        li3 = document.getElementById('li3'),
+        li4 = document.getElementById('li4');
+
+    var crumb1 = document.getElementById('crumb1'),
+        crumb2 = document.getElementById('crumb2'),
+        crumb3 = document.getElementById('crumb3'),
+        crumb4 = document.getElementById('crumb4');
+
+
+
+    function team(li, crumb) {
+      li.onmouseover = function () {
+        crumb.style.width = '15px';
+        crumb.style.height = '15px';
+        crumb.style.background = '#f7600e';
+      };
+
+      li.onmouseout = function () {
+        crumb.style.width = '';
+        crumb.style.height = '';
+        crumb.style.background = '';
+      };
+    }
+
+    team(li1, crumb1);
+    team(li2, crumb2);
+    team(li3, crumb3);
+    team(li4, crumb4);
 
 })();
